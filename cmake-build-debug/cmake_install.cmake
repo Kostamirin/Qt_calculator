4338468865d72b85b1239409aa1c540e88f7f9fc
KerasinChick <base@gmain.com>
2025-03-30 08:13:27 +0300

    Remove outdated build files and update project paths
    Deleted old CMake and build output files related to the QT_GUI project. Updated project paths to transition from "QT_GUI_project" to "Qt_calculator". This cleans up redundant files and aligns configurations with the new project structure.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/kleymuner2131/CLionProjects/QT_GUI project
+# Install script for directory: /home/kleymuner2131/CLionProjects/Qt_calculator
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -57,6 +57,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/home/kleymuner2131/CLionProjects/QT_GUI project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/kleymuner2131/CLionProjects/Qt_calculator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
