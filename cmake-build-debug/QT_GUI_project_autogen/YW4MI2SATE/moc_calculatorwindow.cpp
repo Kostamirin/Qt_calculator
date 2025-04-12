4691265acc38e63784b91e1ce528850eb83d3551
KerasinChick <base@gmain.com>
2025-04-12 14:32:32 +0300

    create main-string little void's changes __update

--- cmake-build-debug/QT_GUI_project_autogen/YW4MI2SATE/moc_calculatorwindow.cpp
+++ cmake-build-debug/QT_GUI_project_autogen/YW4MI2SATE/moc_calculatorwindow.cpp
@@ -27,37 +27,70 @@ QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 namespace {
 struct qt_meta_stringdata_CalculatorWindow_t {
-    uint offsetsAndSizes[16];
+    uint offsetsAndSizes[38];
     char stringdata0[17];
-    char stringdata1[13];
+    char stringdata1[20];
     char stringdata2[1];
-    char stringdata3[16];
-    char stringdata4[14];
-    char stringdata5[13];
-    char stringdata6[20];
-    char stringdata7[17];
+    char stringdata3[11];
+    char stringdata4[11];
+    char stringdata5[11];
+    char stringdata6[11];
+    char stringdata7[13];
+    char stringdata8[13];
+    char stringdata9[17];
+    char stringdata10[17];
+    char stringdata11[19];
+    char stringdata12[18];
+    char stringdata13[18];
+    char stringdata14[17];
+    char stringdata15[19];
+    char stringdata16[19];
+    char stringdata17[18];
+    char stringdata18[18];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CalculatorWindow_t::offsetsAndSizes) + ofs), len 
 Q_CONSTINIT static const qt_meta_stringdata_CalculatorWindow_t qt_meta_stringdata_CalculatorWindow = {
     {
         QT_MOC_LITERAL(0, 16),  // "CalculatorWindow"
-        QT_MOC_LITERAL(17, 12),  // "digitClicked"
-        QT_MOC_LITERAL(30, 0),  // ""
-        QT_MOC_LITERAL(31, 15),  // "operatorClicked"
-        QT_MOC_LITERAL(47, 13),  // "equalsClicked"
-        QT_MOC_LITERAL(61, 12),  // "clearClicked"
-        QT_MOC_LITERAL(74, 19),  // "secondButtonClicked"
-        QT_MOC_LITERAL(94, 16)   // "oneButtonClicked"
+        QT_MOC_LITERAL(17, 19),  // "secondButtonClicked"
+        QT_MOC_LITERAL(37, 0),  // ""
+        QT_MOC_LITERAL(38, 10),  // "sumClicked"
+        QT_MOC_LITERAL(49, 10),  // "subClicked"
+        QT_MOC_LITERAL(60, 10),  // "mulClicked"
+        QT_MOC_LITERAL(71, 10),  // "divClicked"
+        QT_MOC_LITERAL(82, 12),  // "equalClicked"
+        QT_MOC_LITERAL(95, 12),  // "clearClicked"
+        QT_MOC_LITERAL(108, 16),  // "oneButtonClicked"
+        QT_MOC_LITERAL(125, 16),  // "twoButtonClicked"
+        QT_MOC_LITERAL(142, 18),  // "threeButtonClicked"
+        QT_MOC_LITERAL(161, 17),  // "fourButtonClicked"
+        QT_MOC_LITERAL(179, 17),  // "fiveButtonClicked"
+        QT_MOC_LITERAL(197, 16),  // "sixButtonClicked"
+        QT_MOC_LITERAL(214, 18),  // "sevenButtonClicked"
+        QT_MOC_LITERAL(233, 18),  // "eightButtonClicked"
+        QT_MOC_LITERAL(252, 17),  // "nineButtonClicked"
+        QT_MOC_LITERAL(270, 17)   // "zeroButtonClicked"
     },
     "CalculatorWindow",
-    "digitClicked",
-    "",
-    "operatorClicked",
-    "equalsClicked",
-    "clearClicked",
     "secondButtonClicked",
-    "oneButtonClicked"
+    "",
+    "sumClicked",
+    "subClicked",
+    "mulClicked",
+    "divClicked",
+    "equalClicked",
+    "clearClicked",
+    "oneButtonClicked",
+    "twoButtonClicked",
+    "threeButtonClicked",
+    "fourButtonClicked",
+    "fiveButtonClicked",
+    "sixButtonClicked",
+    "sevenButtonClicked",
+    "eightButtonClicked",
+    "nineButtonClicked",
+    "zeroButtonClicked"
 };
 #undef QT_MOC_LITERAL
 } // unnamed namespace
@@ -68,7 +101,7 @@ Q_CONSTINIT static const uint qt_meta_data_CalculatorWindow[] = {
       10,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+      17,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -76,14 +109,36 @@ Q_CONSTINIT static const uint qt_meta_data_CalculatorWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   50,    2, 0x08,    1 /* Private */,
-       3,    0,   51,    2, 0x08,    2 /* Private */,
-       4,    0,   52,    2, 0x08,    3 /* Private */,
-       5,    0,   53,    2, 0x08,    4 /* Private */,
-       6,    0,   54,    2, 0x08,    5 /* Private */,
-       7,    0,   55,    2, 0x08,    6 /* Private */,
+       1,    0,  116,    2, 0x08,    1 /* Private */,
+       3,    0,  117,    2, 0x08,    2 /* Private */,
+       4,    0,  118,    2, 0x08,    3 /* Private */,
+       5,    0,  119,    2, 0x08,    4 /* Private */,
+       6,    0,  120,    2, 0x08,    5 /* Private */,
+       7,    0,  121,    2, 0x08,    6 /* Private */,
+       8,    0,  122,    2, 0x08,    7 /* Private */,
+       9,    0,  123,    2, 0x08,    8 /* Private */,
+      10,    0,  124,    2, 0x08,    9 /* Private */,
+      11,    0,  125,    2, 0x08,   10 /* Private */,
+      12,    0,  126,    2, 0x08,   11 /* Private */,
+      13,    0,  127,    2, 0x08,   12 /* Private */,
+      14,    0,  128,    2, 0x08,   13 /* Private */,
+      15,    0,  129,    2, 0x08,   14 /* Private */,
+      16,    0,  130,    2, 0x08,   15 /* Private */,
+      17,    0,  131,    2, 0x08,   16 /* Private */,
+      18,    0,  132,    2, 0x08,   17 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -103,17 +158,39 @@ Q_CONSTINIT const QMetaObject CalculatorWindow::staticMetaObject = { {
     qt_incomplete_metaTypeArray<qt_meta_stringdata_CalculatorWindow_t,
         // Q_OBJECT / Q_GADGET
         QtPrivate::TypeAndForceComplete<CalculatorWindow, std::true_type>,
-        // method 'digitClicked'
+        // method 'secondButtonClicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'operatorClicked'
+        // method 'sumClicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'equalsClicked'
+        // method 'subClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'mulClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'divClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'equalClicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'clearClicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'secondButtonClicked'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'oneButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'twoButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'threeButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'fourButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'fiveButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'sixButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'sevenButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'eightButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'nineButtonClicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'zeroButtonClicked'
         QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
@@ -125,12 +202,23 @@ void CalculatorWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int
         auto *_t = static_cast<CalculatorWindow *>(_o);
         (void)_t;
         switch (_id) {
-        case 0: _t->digitClicked(); break;
-        case 1: _t->operatorClicked(); break;
-        case 2: _t->equalsClicked(); break;
-        case 3: _t->clearClicked(); break;
-        case 4: _t->secondButtonClicked(); break;
-        case 5: _t->oneButtonClicked(); break;
+        case 0: _t->secondButtonClicked(); break;
+        case 1: _t->sumClicked(); break;
+        case 2: _t->subClicked(); break;
+        case 3: _t->mulClicked(); break;
+        case 4: _t->divClicked(); break;
+        case 5: _t->equalClicked(); break;
+        case 6: _t->clearClicked(); break;
+        case 7: _t->oneButtonClicked(); break;
+        case 8: _t->twoButtonClicked(); break;
+        case 9: _t->threeButtonClicked(); break;
+        case 10: _t->fourButtonClicked(); break;
+        case 11: _t->fiveButtonClicked(); break;
+        case 12: _t->sixButtonClicked(); break;
+        case 13: _t->sevenButtonClicked(); break;
+        case 14: _t->eightButtonClicked(); break;
+        case 15: _t->nineButtonClicked(); break;
+        case 16: _t->zeroButtonClicked(); break;
         default: ;
         }
     }
@@ -156,13 +244,13 @@ int CalculatorWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 17)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 17;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 17)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 6;
+        _id -= 17;
     }
     return _id;
 }
