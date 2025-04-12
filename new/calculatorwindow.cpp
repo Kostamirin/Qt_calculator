a0aab31df3b0391a63f592271d9cc6703bed42b1
KerasinChick <base@gmain.com>
2025-04-12 20:08:30 +0300

    close

--- new/calculatorwindow.cpp
+++ new/calculatorwindow.cpp
@@ -61,6 +61,7 @@ CalculatorWindow::CalculatorWindow(QWidget *parent):QMainWindow(parent)
     QPushButton *open = new QPushButton("("); //todo: Создать функционал для этих скобок
     connect(open, &QPushButton::clicked, this, &CalculatorWindow::openButtonClicked);
     QPushButton *close = new QPushButton(")");
+    connect(close, &QPushButton::clicked, this, &CalculatorWindow::closeButtonClicked);
 
 
     // --- Placeholder for buttons ---
