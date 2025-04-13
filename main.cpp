6ac4067a643b25e08bf26fc544e2c0d5d1f9a6bd
KerasinChick <base@gmain.com>
2025-04-13 18:21:54 +0300

    main.cpp styleFIle return __update

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Load and apply styles
+    QFile styleFile(":/calculator.qss");
+    if (!styleFile.open(QFile::ReadOnly)) {
+        qWarning("Cannot open style file");
+    } else {
+        QString styleSheet = QString::fromUtf8(styleFile.readAll());
+        a.setStyleSheet(styleSheet);
+        styleFile.close();
+    }
+
     CalculatorWindow w;
     w.show();
     return a.exec();
