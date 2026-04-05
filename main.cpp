0629705fb3305367e3d75bc246fb96e7543b8a82
kleymuner <kostavlasov834@gmail.com>
2026-04-05 15:07:46 +0300

    todo

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 int main(int argc, char *argv[])
 {
     // Create the application object
+    // todo создать глобальные переменные для сохранения введенного текста на панели
+    // todo создать историю 
     QApplication mainApp(argc, argv);
 
     mainWindow window;
